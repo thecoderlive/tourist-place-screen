a268b236eb224191baf59ed0909ed70067acb390
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-12 14:46:27 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,32 @@
+export const item = {
+    "id": "81228845-6628-4f41-aaa2-1e56aa4d87d2",
+    "company_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png",
+    "place_title": "push",
+    "places_list": [
+        {
+            "id": "c3ae8017-986f-4a77-b5d9-08f6a8b150b9",
+            "place_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/314.png",
+            "place_text": "model"
+        },
+        {
+            "id": "d7a54c2c-0a0a-4b3d-8ae8-32021081482e",
+            "place_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png",
+            "place_text": "begin"
+        },
+        {
+            "id": "1cb78e58-de47-4421-900c-1ca3dc61b2f5",
+            "place_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png",
+            "place_text": "within"
+        },
+        {
+            "id": "538195c9-8a34-439a-854e-daf8a985f477",
+            "place_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png",
+            "place_text": "consumer"
+        },
+        {
+            "id": "822e512c-bc4b-42fc-b559-a81ccf9f6708",
+            "place_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png",
+            "place_text": "agent"
+        }
+    ]
+}
